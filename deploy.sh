f5eb7cb6d33dee2573dbb5e93e5407bad7e2c1a0
Anton Kostin <kostin_anton@bk.ru>
2017-12-27 21:58:40 +0300

    Basic HW-06

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home/appuser/
+git clone https://github.com/Otus-DevOps-2017-11/reddit.git
+##Переходим в директорию проекта и устанавливаем зависимости приложения
+#cd reddit && bundle install
+
+##Запускаем сервер приложения в папке проекта
+puma -d
+
---
+++ install_ruby.sh
@@ -0,0 +1,5 @@
+##Обновляем APT
+sudo apt update
+##устанавливаем Ruby и Bundler
+sudo apt install -y ruby-full ruby-bundler build-essential
+
