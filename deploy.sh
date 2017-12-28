a195ba8ff24db31e2aa941f364227c0b3af73fcc
Anton Kostin <kostin_anton@bk.ru>
2017-12-29 00:55:03 +0300

    removed comment for bundle install line

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 cd /home/appuser/
 git clone https://github.com/Otus-DevOps-2017-11/reddit.git
 ##Переходим в директорию проекта и устанавливаем зависимости приложения
-#cd reddit && bundle install
+cd reddit && bundle install
 
 ##Запускаем сервер приложения в папке проекта
 puma -d
